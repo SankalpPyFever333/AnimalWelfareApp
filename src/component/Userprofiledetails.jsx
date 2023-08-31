70ab522540b0d4df2a3465e08062431802f9db77
sankalp <sankalppandey696@gmail.com>
2023-08-31 19:52:54 +0530

    user can upload his picture now

--- src/component/Userprofiledetails.jsx
+++ src/component/Userprofiledetails.jsx
@@ -1,15 +1,37 @@
-import React ,{useState} from 'react'
+import React ,{useRef ,useState} from 'react'
 import "./userprofile.css"
+import { red } from '@mui/material/colors';
 function Userprofiledetails() {
   const [File, setFile] = useState('');
+  const inputref = useRef(null);
 
-  const convertBase64 = (File)=>{
-    
+
+  const handleImageClick = ()=>{
+    inputref.current.click();
+  }
+
+  const handleImageChange = (e)=>{
+    console.log("File event", e.target.files);
+    //  it contains several details like lastModified, date, type , path, name etc.
+    const file = e.target.files[0];
+    console.log(file);
+    setFile(file);
   }
   
   return (
     <div>
-      <h1>User profile details</h1>
+      <div className="userImagePrfile" onClick={handleImageClick}>
+        <img  src="./assests/profileIcon.jpeg" alt="" style={{border:"3px solid red",width:"35vw", height:"16vh"}} />
+        {
+          File ? (
+            <img src= {URL.createObjectURL(File)} alt="" />
+          ) : 
+          (
+            <img src="./assests/profileIcon.jpeg" alt="Icon" />
+          )
+        }
+        <input type="File" onChange={handleImageChange} ref={inputref} style={{display:"none"}} />
+      </div>
     </div>
   )
 }
